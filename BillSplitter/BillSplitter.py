29ebcff9f52b51d19c0073498e7382314b10ae57
wongko <wongkoty@gmail.com>
2018-10-02 16:47:49 -0400

    -deleting files

--- BillSplitter/BillSplitter.py
+++
@@ -1,8 +0,0 @@
-
-def main():
-    total = int(input("Enter Bill Total: "))
-    people = int(input("Enter Number of people: "))
-    split = total/people
-    print('Each person pays ' + str(split) + ' dollars')
-
-main()
