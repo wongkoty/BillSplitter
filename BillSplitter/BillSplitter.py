7e0ffeaa6966585046fb9ebecc0344bf66ba2b65
Albert Xing <albertxing@Alberts-MacBook-Pro-4.local>
2018-10-02 14:22:27 -0400

    Add BillSplitter/BillSplitter.py

--- BillSplitter/BillSplitter.py
+++ BillSplitter/BillSplitter.py
@@ -1,4 +1,15 @@
-total = int(input("Enter Bill Total: "))
+total = float(input("Enter Bill Total: "))
 people = int(input("Enter Number of people: "))
-split = total/people
-print('Each person pays ' + str(split) + ' dollars')
+cash_credit = input('Cash or Credit?: ')
+total_tax_adj = total * 1.08875
+tips = total * 0.2
+split = total_tax_adj/people
+if str(cash_credit) == 'credit' or 'Credit':      
+        min = input('Credit Card Minimum?: ')
+        if int(split) < int(min):
+                print("Not enough for credit")
+        else:
+                print('Each person pays ' + str(split) + ' dollars' 'and tips ' + str(tips))
+        
+else:
+        print('Each person pays ' + str(split) + ' dollars' 'and tips ' + str(tips))
