d0602b018fd37f122d5ae70d647698415a3e5655
Albert Xing <albertxing@Alberts-MacBook-Pro-4.local>
2018-10-02 13:10:17 -0400

    Add BillSplitter/BillSplitter.py

--- BillSplitter/BillSplitter.py
+++ BillSplitter/BillSplitter.py
@@ -1,2 +1,4 @@
-
-print("Hello Koty")
+total = int(input("Enter Bill Total: "))
+people = int(input("Enter Number of people: "))
+split = total/people
+print('Each person pays ' + str(split) + ' dollars')
